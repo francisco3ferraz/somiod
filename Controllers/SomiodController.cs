4dbe7003ac45c01f4961022db4a4f52981096d51
Francisco Ferraz <2230706@my.ipleiria.pt>
2025-11-14 19:09:33 +0000

    Integrated Swagger for API documentation and added a Web API controller with a placeholder for CRUD operations.

---
+++ Controllers/SomiodController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using SOMIOD.Models;
+
+namespace SOMIOD.Controllers
+{
+    [RoutePrefix("api/somiod")]
+    public class SomiodController : ApiController
+    {
+        private string connectionString =
+            SOMIOD.Properties.Settings.Default.ConnStr;
+
+        // TODO: Implement CRUD operations for all resources
+        // Following the RESTful patterns from Worksheet 03
+    }
+}
