e52c75d9a173815ca278aff551e6b1be207bfa87
Francisco Ferraz <2230706@my.ipleiria.pt>
2025-12-21 21:29:45 +0000

    ...

--- Models/Subscription.cs
+++ Models/Subscription.cs
@@ -4,7 +4,7 @@ namespace SOMIOD.Models
 {
     public class Subscription : ResourceBase
     {
-        public int evt { get; set; } // 1=creation, 2=deletion
+        public int evt { get; set; } // 1=creation, 2=deletion, 3=both
         public string endpoint { get; set; }
 
         public Subscription()
